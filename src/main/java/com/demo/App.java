82f29dfa6592690e329f2178fd790db2ed86f95b
sizukutamago <tanaka@tam-bourine.co.jp>
2019-10-29 15:46:01 +0900

    annotation bean

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -3,13 +3,16 @@ package com.demo;
 import com.demo.bean.MyBean;
 import com.demo.bean.MyBeanInterface;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     private static ApplicationContext app;
     public static void main(String[] arg) {
-        app = new ClassPathXmlApplicationContext("bean.xml");
-        MyBeanInterface bean = (MyBeanInterface) app.getBean("mybean1");
+//        app = new ClassPathXmlApplicationContext("bean.xml");
+////        MyBeanInterface bean = (MyBeanInterface) app.getBean("mybean1");
+        app = new AnnotationConfigApplicationContext(AutoMyBeanConfig.class);
+        MyBeanInterface bean = app.getBean(MyBeanInterface.class);
         System.out.println(bean);
     }
 }
